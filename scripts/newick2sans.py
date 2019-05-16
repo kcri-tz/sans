2a198aeb1bb79d168067a8c0f3e9474f4c5009a8
rwittler <roland.wittler@uni-bielefeld.de>
2019-05-16 10:13:56 +0200

    no output of empty root edge

--- scripts/newick2sans.py
+++ scripts/newick2sans.py
@@ -18,21 +18,9 @@ tree = dendropy.Tree.get(
         path=sys.argv[1],
         schema="newick",
         preserve_underscores=True)
-eprint(len(tree.leaf_nodes()))
 
+eprint("number of nodes read: "+ str(len(tree.leaf_nodes())))
 
-#if len(sys.argv)>2:
-    ## read mapping
-    #mapping=dict()
-    #inv_mapping=dict()
-    #for line in (s.strip() for s in open(sys.argv[2])):
-        #items=line.split('\t')
-        #mapping[items[0]]=items[1]
-        #inv_mapping[items[1]]=items[0]
-    ##prune
-    #node_filter_fn = lambda nd: nd.is_internal() or nd.taxon.label in mapping.keys()
-    #tree = tree.extract_tree(node_filter_fn=node_filter_fn)
-    #eprint(len(tree.leaf_nodes()))
 
 if len(sys.argv)>2:
     eprint("pruning tree...")
@@ -41,30 +29,21 @@ if len(sys.argv)>2:
     taxa=set()
     for line in (s.strip() for s in open(sys.argv[2])):
         taxa.add(line)
-        #if line not in mapping.values():
-            #eprint("taxa from list not in mapping (tree pruned) : "+line)
-        #elif inv_mapping[line] not in leaflables:
-            #eprint("taxa from list not in tree (tree pruned) : "+line+" <- "+inv_mapping[line])
         if line not in leaflables:
             eprint("taxa from list not in tree: "+line)
     eprint("taxa read: "+str(len(taxa)))
-#    for l in leaflables:
-#        if  l not in taxa:
-#            eprint("taxa from tree not in taxa list (tree pruned) : "+l)
     node_filter_fn = lambda nd: nd.is_internal() or nd.taxon.label in taxa
     tree = tree.extract_tree(node_filter_fn=node_filter_fn)
-    #eprint(len(tree.leaf_nodes()))
 
 
 tree.encode_bipartitions(suppress_unifurcations=True,collapse_unrooted_basal_bifurcation=True)
 for edge in tree.preorder_edge_iter():
-#for node in tree:
+    # omit root edge -> split "all vs. none"
+    if len(edge.bipartition.leafset_taxa(tree.taxon_namespace)) == len(tree.leaf_nodes()):
+        continue;
     if edge.length:
         split=str(edge.length)+"\t"
     else:
         split="0.0\t"
-#    if len(sys.argv)==2:
     split+="\t".join([t.label for t in edge.bipartition.leafset_taxa(tree.taxon_namespace)])
-#    else:
-#        split+="\t".join(map(map_taxa,[t.label for t in edge.bipartition.leafset_taxa(tree.taxon_namespace)]))
     print(split)
