8ac50f2739afb9b82a8af8eee93b6ff074cd60e4
rwittler <roland.wittler@uni-bielefeld.de>
2019-06-13 16:15:53 +0200

    switched from wget to curl

--- example_data/prasinoviruses/download.sh
+++ example_data/prasinoviruses/download.sh
@@ -5,7 +5,7 @@ cat ../IDs.txt | while read l;
 do
 	NC=$(echo $l | cut -f1 -d" "); 
 	TAX=$(echo $l | cut -f2 -d " ");
-	wget -O "$TAX.fasta" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=$NC&rettype=fasta"
+	curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=$NC&rettype=fasta" > "$TAX.fasta"
         echo "$TAX.fasta" >> list.txt
 done;
 cd ..;
