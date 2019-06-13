863e39bcc359facbeb029467a8555ce4d5eede38
rwittler <roland.wittler@uni-bielefeld.de>
2019-06-13 13:50:58 +0200

    Mozilla::CA required for efetch

--- scripts/download_ncbi.pl
+++ scripts/download_ncbi.pl
@@ -1,3 +1,4 @@
 #!/usr/bin/perl
 use LWP::Simple;
-print get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=".$ARGV[0]."&rettype=fasta");
+use Mozilla::CA;
+getprint("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=".$ARGV[0]."&rettype=fasta");
