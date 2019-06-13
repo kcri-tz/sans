d2ef6a5a82718dd6452122253acd838efa69f3cf
rwittler <roland.wittler@uni-bielefeld.de>
2019-06-13 16:13:00 +0200

    simplified download script for prasinovirus data

--- example_data/prasinoviruses/download.sh
+++ example_data/prasinoviruses/download.sh
@@ -4,8 +4,8 @@ rm -f list.txt;
 cat ../IDs.txt | while read l;
 do
 	NC=$(echo $l | cut -f1 -d" "); 
-	TAX=$(echo $l | cut -f2 -d " "); 
-        ../../../scripts/download_ncbi.pl $NC > $TAX.fasta;
+	TAX=$(echo $l | cut -f2 -d " ");
+	wget -O "$TAX.fasta" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=$NC&rettype=fasta"
         echo "$TAX.fasta" >> list.txt
 done;
 cd ..;
--- scripts/download_ncbi.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl
-use LWP::Simple;
-use Mozilla::CA;
-getprint("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=".$ARGV[0]."&rettype=fasta");
