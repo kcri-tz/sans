c51a23794c280b064801389ac55904bd64d3cfbe
Roland Wittler <roland.wittler@uni-bielefeld.de>
2021-01-19 10:51:33 +0100

    Update download_CDS.sh

--- example_data/drosophila/download_CDS.sh
+++ example_data/drosophila/download_CDS.sh
@@ -1,9 +1,7 @@
-mkdir -p fa;
-cd fa;
 mkdir -p CDS;
 cd CDS;
-for f in `cat ../../download_CDS.list`; do wget $f; done;
+for f in `cat ../download_CDS.list`; do wget $f; done;
 for f in *.gz; do gunzip $f; done;
 for f in *.fasta; do nf=$(echo $f | sed -E 's/d(...).*\.fasta/\1.fasta/g'); mv $f $nf; done;
 ls *.fasta > list_CDS.txt;
-cd ../..
+cd ..
