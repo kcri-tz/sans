4fb144a98b9759abce9276787fbd0a7890a90629
Roland Wittler <roland.wittler@uni-bielefeld.de>
2021-01-20 13:56:54 +0100

    Update download_WG.sh

--- example_data/drosophila/download_WG.sh
+++ example_data/drosophila/download_WG.sh
@@ -3,5 +3,5 @@ cd WG;
 for f in `cat ../download_WG.list`; do wget $f; done;
 for f in *.gz; do gunzip $f; done;
 for f in *.fasta; do nf=$(echo $f | sed -E 's/d(...).*\.fasta/\1.fasta/g'); mv $f $nf; done;
-ls *.fasta > list_WG.txt;
+ls *.fasta > list.txt;
 cd ..
