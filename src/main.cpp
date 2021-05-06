a2327d32a4021598665a1b452b81373371d793f0
Fabian Kolesch <fabian.kolesch@uni-bielefeld.de>
2021-05-06 17:56:37 +0200

    [Updated main.cpp] name table fix #3

--- src/main.cpp
+++ src/main.cpp
@@ -412,8 +412,8 @@ int main(int argc, char* argv[]) {
 
         vector<string> cdbg_names = cdbg.getColorNames(); // color names of the cdbg compacted genomes.
         for (auto it=cdbg_names.begin(); it != cdbg_names.end(); ++it){ // iterate the cdbg names and transcribe them to the name table
-            name_table[cdbg_names[it]] = ++num;
-            denom_names.push_back(cdbg_names[it]);
+            name_table[it] = ++num;
+            denom_names.push_back(it);
         }
 
         if (num > maxN) {
