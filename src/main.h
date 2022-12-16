992a6d2deabd6b0e1c7556e5fd8b3d1fac176ec5
Roland Wittler <roland.wittler@uni-bielefeld.de>
2022-12-16 09:32:59 +0100

    Update version

--- src/main.h
+++ src/main.h
@@ -19,7 +19,7 @@ using namespace std;
 
 // Symmetric Alignment-free phylogeNomic Splits
 // simple efficient re-implementation + filters
-#define SANS_VERSION "2.2_11B"    // SANS serif
+#define SANS_VERSION "2.2_12A"    // SANS serif
 
 /**
  * This is the entry point of the program.
