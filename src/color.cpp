4c99a9c1f75e3adf1aa675a3bbe919e1012f53f0
fabian kolesch <fabian.kolesch@uni-bielefeld.de>
2023-03-01 16:45:44 +0100

    [BUILD] Merged new colors and byte

---
+++ src/color.cpp
@@ -0,0 +1,92 @@
+#include "color.h"
+
+/*
+ * This class contains functions for working with color types.
+ */
+size1N_t color::n;      // number of colors
+color_t  color::mask;   // bit-mask to erase all bits that exceed the color number
+
+/**
+ * This function initializes the color number and bit-mask.
+ *
+ * @param number color number
+ */
+void color::init(const size1N_t& number) {
+    n = number; mask = 0b0u;
+    for (size1N_t i = 0; i < n; ++i)  // fill all bits within the color number with ones
+        (mask <<= 01u) |= 0b1u;      // the remaining zero bits can be used to mask bits
+}
+
+/**
+ * This function shifts a color appending a new bit char to the right.
+ *
+ * @param color bit sequence
+ * @param chr right color bit
+ */
+void color::shift(color_t& color, const char& chr) {
+    color <<= 01u;    // shift all current bits to the left by one position
+    color |= chr-48;    // encode the new rightmost color bit char
+    color &= mask;    // set all bits to zero that exceed the color number
+}
+
+/**
+ * This function unshifts a color returning the bit char on the right.
+ *
+ * @param color bit sequence
+ * @param chr right color bit
+ */
+void color::unshift(color_t& color, char& chr) {
+    chr = (color & 0b1u)+48;    // return the rightmost color bit char
+    color >>= 01u;    // shift all current bits to the right by one position
+//  color &= mask;    // set all bits to zero that exceed the color number
+}
+
+/**
+ * This function constructs the bit complement of a given color set.
+ *
+ * @param color bit sequence
+ */
+void color::complement(color_t& color) {
+    color = ~color & mask;    // flip the bits
+}
+
+/**
+ * This function constructs the representative of a given color set.
+ *
+ * @param color bit sequence
+ * @return 1 if inverted, 0 otherwise
+ */
+bool color::represent(color_t& color) {
+    size1N_t count = color.popcnt();
+    // return the color set with fewer ones to represent the split
+    if (2*count < n || 2*count == n && (color & 0b1u))
+        return false;    // not inverted
+    else color = ~color & mask;    // flip the bits
+        return true;    // inverted
+}
+
+/**
+ * This function tests if two splits of colors are compatible.
+ *
+ * @param c1 bit sequence
+ * @param c2 bit sequence
+ * @return true, if compatible
+ */
+bool color::is_compatible(const color_t& c1, const color_t& c2) {
+    color_t n1 = ~c1 & mask, n2 = ~c2 & mask; using _ = color_t;
+    return (_::disjoint(c1, c2) || _::disjoint(c1, n2) || _::disjoint(n1, c2) || _::disjoint(n1, n2));
+}
+
+/**
+ * This function tests if three splits of colors are weakly compatible.
+ *
+ * @param c1 bit sequence
+ * @param c2 bit sequence
+ * @param c3 bit sequence
+ * @return true, if weakly compatible
+ */
+bool color::is_weakly_compatible(const color_t& c1, const color_t& c2, const color_t& c3) {
+    color_t n1 = ~c1 & mask, n2 = ~c2 & mask, n3 = ~c3 & mask; using _ = color_t;
+    return (_::disjoint(c1, c2, c3) || _::disjoint(c1, n2, n3) || _::disjoint(n1, c2, n3) || _::disjoint(n1, n2, c3))
+        && (_::disjoint(n1, n2, n3) || _::disjoint(n1, c2, c3) || _::disjoint(c1, n2, c3) || _::disjoint(c1, c2, n3));
+}
---
+++ src/color.h
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+#ifndef maxN     // max. color number defined
+#define maxN 64  // as preprocessor directive
+#endif
+
+#define CLASS_NAME   color_t
+#define STORAGE_TYPE uint1N_t
+#define INDEX_TYPE   size1N_t
+#define BIT_LENGTH   (1*maxN)
+#define SET_ELEMENT_COMPARATORS
+#include "byte.h"
+
+/**
+ * This class contains functions for working with color types.
+ */
+class color {
+
+ private:
+
+    /**
+     * This is a bit-mask to erase all bits that exceed the color number.
+     */
+    static color_t mask;
+
+ public:
+
+    /**
+     * This is the number of colors.
+     */
+    static size1N_t n;
+
+    /**
+     * This function initializes the color number and bit-mask.
+     *
+     * @param number color number
+     */
+    static void init(const size1N_t& number);
+
+    /**
+     * This function shifts a color appending a new bit char to the right.
+     *
+     * @param color bit sequence
+     * @param chr right color bit
+     */
+    static void shift(color_t& color, const char& chr);
+
+    /**
+     * This function unshifts a color returning the bit char on the right.
+     *
+     * @param color bit sequence
+     * @param chr right color bit
+     */
+    static void unshift(color_t& color, char& chr);
+
+    /**
+     * This function constructs the bit complement of a given color set.
+     *
+     * @param color bit sequence
+     */
+    static void complement(color_t& color);
+
+    /**
+     * This function constructs the representative of a given color set.
+     *
+     * @param color bit sequence
+     * @return 1 if inverted, 0 otherwise
+     */
+    static bool represent(color_t& color);
+
+    /**
+     * This function tests if two splits of colors are compatible.
+     *
+     * @param c1 bit sequence
+     * @param c2 bit sequence
+     * @return true, if compatible
+     */
+    static bool is_compatible(const color_t& c1, const color_t& c2);
+
+    /**
+     * This function tests if three splits of colors are weakly compatible.
+     *
+     * @param c1 bit sequence
+     * @param c2 bit sequence
+     * @param c3 bit sequence
+     * @return true, if weakly compatible
+     */
+    static bool is_weakly_compatible(const color_t& c1, const color_t& c2, const color_t& c3);
+
+ protected:
+
+};
