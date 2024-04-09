42dbe7cc3906fced14d3539c6ac583f344344aa5
Roland Wittler <roland.wittler@uni-bielefeld.de>
2024-04-09 09:17:57 +0200

    fixed: static quality value was not set (problem for clear_thread)

--- src/graph.cpp
+++ src/graph.cpp
@@ -209,6 +209,7 @@ void graph::init(uint64_t& top_size, bool amino, vector<int>& q_table, int& qual
         graph::allowedChars.push_back('*');
     }
 
+    graph::quality = quality;
     graph::q_table = q_table;
     switch (quality) {
     case 1:
